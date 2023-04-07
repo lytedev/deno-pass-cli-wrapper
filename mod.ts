526355b7bf95765c50f6d362eb17c011496da16e
Daniel Flanagan <daniel@lyte.dev>
2023-04-07 17:02:52 -0500

    Initial commit

---
+++ mod.ts
@@ -0,0 +1,87 @@
+async function passOutput(args: string[]): Promise<string | undefined> {
+  const command = Deno.run({
+    cmd: ["pass", ...args],
+    stdout: "piped",
+    stdin: "null",
+    stderr: "null",
+  });
+  const status = await command.status();
+  if (!status.success) {
+    return;
+  }
+  const output = await command.output();
+  return new TextDecoder().decode(output);
+}
+
+/**
+ * Gets entire (trimmed) contents from the specified entry in the password store.
+ */
+export async function entryContents(
+  entry: string,
+): Promise<string | undefined> {
+  return (await passOutput([entry]))?.trim();
+}
+
+/**
+ * Gets the first non-empty line from the specified entry in the password store.
+ */
+export async function passwordFor(entry: string): Promise<string | undefined> {
+  return (await passOutput([entry]))?.trim().split("\n")[0];
+}
+
+/**
+ * Equivalent to retrieving the contents and looking for a line that starts with
+ * `${fieldName}:` (the fieldName argument followed by a colon). The remainder
+ * of the line (everything after the first colon) will be trimmed and returned.
+ *
+ * For example, if you run `pass google.com/personal` and get the following output:
+ *
+ * hunter2
+ * username: johnsmith
+ * full_email:johnsmith@gmail.com
+ * app_password:    abcd 1234 efgh 5678
+ *
+ * Then `fieldFor("google.com/personal", "app_password")` would return `abcd 1234 efgh 5678`.
+ */
+export async function fieldFor(
+  entry: string,
+  fieldName: string,
+): Promise<string | undefined> {
+  const scan = `${fieldName}:`;
+  const lines = (await passOutput([entry]))?.trim().split("\n");
+  if (lines === undefined) return;
+  for (const line of lines) {
+    if (line.trim().startsWith(scan)) {
+      return line.substring(scan.length).trim();
+    }
+  }
+  return;
+}
+
+/*
+// Maybe someday we'll get fancy!
+
+export class InvocationResult {
+  constructor(opts,
+}
+
+export type FileDescriptor = "inherit" | "piped" | "null" | number;
+
+export interface InvokeInit {
+  args?: string[],
+  stdout?: FileDescriptor,
+  stderr?: FileDescriptor,
+  stdin?: FileDescriptor,
+}
+
+export async function rawInvoke(opts: InvokeInit): Promise<InvocationResult> {
+  const { args, stdout, stderr, stdin } = opts;
+  const run = Deno.run({
+    cmd: ["pass"].concat(args || []),
+    stdout: stdout || "piped",
+    stdin: stdin || "null",
+    stderr: stderr || "piped",
+  });
+  return new InvocationResult(opts, run.status(), run.stdout, run.stderr);
+}
+*/
